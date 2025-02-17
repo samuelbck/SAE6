2585ba109945bac08c1e0261eb8be8dc77215225
Samuel Binckly <samuelbinckly@gmail.com>
2025-02-17 15:53:27 +0100

    Creation du modèle et de la table d'historique

--- sae_app_mobile/lib/models/plant.dart
+++ sae_app_mobile/lib/models/plant.dart
@@ -1,1 +1,52 @@
 // Modele des données de la plante
+import 'dart:typed_data';
+
+class Plant {
+  final int? id;
+  final String name;
+  final double latitude;
+  final double longitude;
+  final double predictionScore;
+  final Uint8List image;
+  final String url;
+  final DateTime timestamp;
+
+  Plant({
+    this.id,
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+    required this.predictionScore,
+    required this.image,
+    required this.url,
+    required this.timestamp,
+  });
+
+  // Convertion pour l'insertion dans la base
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'latitude': latitude,
+      'longitude': longitude,
+      'predictionScore': predictionScore,
+      'image': image,
+      'url': url,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  // Créer un objet Plant depuis la base de données
+  factory Plant.fromMap(Map<String, dynamic> map) {
+    return Plant(
+      id: map['id'],
+      name: map['name'],
+      latitude: map['latitude'],
+      longitude: map['longitude'],
+      predictionScore: map['predictionScore'],
+      image: Uint8List.fromList(map['image']),
+      url: map['url'],
+      timestamp: DateTime.parse(map['timestamp']),
+    );
+  }
+}
--- sae_app_mobile/lib/services/database_service.dart
+++ sae_app_mobile/lib/services/database_service.dart
@@ -2,6 +2,9 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:convert';
+import 'dart:typed_data';
 
 class DatabaseService {
   static final DatabaseService _instance = DatabaseService._internal();
@@ -36,10 +39,15 @@ class DatabaseService {
   Future<void> _onCreate(Database db, int version) async {
     // Créer la table
     await db.execute('''
-      CREATE TABLE items(
+      CREATE TABLE historique (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        value INTEGER
+        latitude REAL,
+        longitude REAL,
+        prediction_score REAL,
+        image BLOB,
+        url TEXT,
+        timestamp TEXT
       )
     ''');
 
@@ -47,7 +55,9 @@ class DatabaseService {
   }
 
   Future<void> _insertInitialData(Database db) async {
-    await db.insert('items', {'id': 1, 'name': 'ben', 'value': 5});
+    String base64String = dotenv.env['IMAGE_BASE64']!;
+    Uint8List imageBytes = base64Decode(base64String);
+    await db.insert('historique', {'id': 1, 'name': 'Tournesol', 'latitude': 48.289919, 'longitude': 6.941942, 'prediction_score': 0.99, 'image': imageBytes, 'url': 'https://plant.id/media/imgs/28f6894ea6384b10b81fd8167ea5f3ee.jpg','timestamp': '2025-02-17'});
     print('Inserted initial data into the database');
   }
 
