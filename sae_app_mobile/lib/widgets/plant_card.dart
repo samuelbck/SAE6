8a32856145550e8f24bf64c6d1e8ba5e8ae35619
Samuel Binckly <samuelbinckly@gmail.com>
2025-02-17 17:07:27 +0100

    Ajout design de la carte d'historique

--- sae_app_mobile/lib/widgets/plant_card.dart
+++ sae_app_mobile/lib/widgets/plant_card.dart
@@ -1,1 +1,28 @@
 // Widget d'une carte de plante (pour l'historique)
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+
+class PlantCard extends StatelessWidget {
+  final String name;
+  final Uint8List image;
+  final String timestamp;
+
+  const PlantCard({
+    Key? key,
+    required this.name,
+    required this.image,
+    required this.timestamp,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: ListTile(
+        leading: Image.memory(image),
+        title: Text(name),
+        subtitle: Text('Date: $timestamp'),
+      ),
+    );
+  }
+}
