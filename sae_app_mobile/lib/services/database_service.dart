45e7f2af5980f70ae5cf92175b20cc79edf1153a
Samuel Binckly <samuelbinckly@gmail.com>
2025-02-17 16:47:22 +0100

    Service de la BDD avec exemple d'insertion

--- sae_app_mobile/lib/services/database_service.dart
+++ sae_app_mobile/lib/services/database_service.dart
@@ -1,9 +1,6 @@
 // Communication avec la base SQLite
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'dart:convert';
 import 'dart:typed_data';
 
 class DatabaseService {
@@ -37,7 +34,6 @@ class DatabaseService {
   }
 
   Future<void> _onCreate(Database db, int version) async {
-    // Créer la table
     await db.execute('''
       CREATE TABLE historique (
         id INTEGER PRIMARY KEY,
@@ -55,20 +51,27 @@ class DatabaseService {
   }
 
   Future<void> _insertInitialData(Database db) async {
-    String base64String = dotenv.env['IMAGE_BASE64']!;
-    Uint8List imageBytes = base64Decode(base64String);
-    await db.insert('historique', {'id': 1, 'name': 'Tournesol', 'latitude': 48.289919, 'longitude': 6.941942, 'prediction_score': 0.99, 'image': imageBytes, 'url': 'https://plant.id/media/imgs/28f6894ea6384b10b81fd8167ea5f3ee.jpg','timestamp': '2025-02-17'});
-    print('Inserted initial data into the database');
+    Map<String, dynamic> initialData = {
+      'name': 'Exemple',
+      'latitude': 48.8566,
+      'longitude': 2.3522,
+      'prediction_score': 0.95,
+      'image': Uint8List.fromList([0, 1, 2, 3]),
+      'url': 'https://example.fr',
+      'timestamp': DateTime.now().toIso8601String(),
+    };
+
+    await db.insert('historique', initialData);
   }
 
   Future<int> insert(Map<String, dynamic> row) async {
     Database? db = await database;
-    return await db!.insert('items', row);
+    return await db!.insert('historique', row);
   }
 
-  Future<List<Map<String, dynamic>>> queryAll() async {
+  Future<List<Map<String, dynamic>>> getAllHistorique() async {
     Database? db = await database;
-    return await db!.query('items');
+    return await db!.query('historique');
   }
 
   bool isWeb() {
