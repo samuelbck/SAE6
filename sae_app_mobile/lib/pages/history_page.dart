13c5ebec476e5378e2376d017b5b8e27a6a9d4c3
Samuel Binckly <samuelbinckly@gmail.com>
2025-02-17 17:08:31 +0100

    Page d'historique simple

--- sae_app_mobile/lib/pages/history_page.dart
+++ sae_app_mobile/lib/pages/history_page.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import '../services/database_service.dart';
+import '../widgets/plant_card.dart';
 
-class HistoryPage extends StatelessWidget {
-  const HistoryPage({Key? key}) : super(key: key);  // Ajout de 'const'
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  List<Map<String, dynamic>> _historique = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHistorique();
+  }
+
+  Future<void> _loadHistorique() async {
+    DatabaseService dbService = DatabaseService();
+    List<Map<String, dynamic>> historique = await dbService.getAllHistorique();
+    setState(() {
+      _historique = historique;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Text(
-        'Page de l\'historique',
-        style: TextStyle(fontSize: 24),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Historique des Plantes'),
+      ),
+      body: ListView.builder(
+        itemCount: _historique.length,
+        itemBuilder: (context, index) {
+          var item = _historique[index];
+          return PlantCard(
+            name: item['name'],
+            image: item['image'],
+            timestamp: item['timestamp'],
+          );
+        },
       ),
     );
   }
