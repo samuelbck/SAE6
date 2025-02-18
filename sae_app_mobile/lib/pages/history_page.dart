10e6055fe30e6b93b6f709aa78788806364cb82b
Samuel Binckly <samuelbinckly@gmail.com>
2025-02-18 14:08:52 +0100

    Amélioration design pour mode sombre et lisibilité

--- sae_app_mobile/lib/pages/history_page.dart
+++ sae_app_mobile/lib/pages/history_page.dart
@@ -34,49 +34,77 @@ class _HistoryPageState extends State<HistoryPage> {
   }
 
   void _showDetailsDialog(Map<String, dynamic> item) {
-    DateTime dateTime = DateTime.parse(item['timestamp']);
-    String formattedDate = DateFormat('dd/MM/yyyy').format(dateTime);
-    String formattedTime = DateFormat('HH:mm').format(dateTime);
-    Color predictionColor = item['prediction_score'] < 50 ? Colors.redAccent : Colors.green;
+  DateTime dateTime = DateTime.parse(item['timestamp']);
+  String formattedDate = DateFormat('dd/MM/yyyy').format(dateTime);
+  String formattedTime = DateFormat('HH:mm').format(dateTime);
 
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(item['name'], style: TextStyle(fontWeight: FontWeight.w600)),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.memory(item['image'], fit: BoxFit.cover),
-              const SizedBox(height: 10),
-              Text('Plante prédite: ${item['name']}', style: TextStyle(fontWeight: FontWeight.w600)),
-              Text('Confiance de la prédiction: ${item['prediction_score']}%', style: TextStyle(color: predictionColor)),
-              Text('Date de la photo: $formattedDate à $formattedTime'),
-              Text('Coordonnées de la photo: ${item['latitude']} ; ${item['longitude']}'),
-            ],
-          ),
-          actions: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.delete, color: Colors.red),
-                  onPressed: () {
-                    _deleteItem(item['id']);
-                    Navigator.pop(context);
-                  },
-                ),
-                TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('Fermer'),
-                ),
-              ],
-            )
+  // Gestion du mode sombre
+  bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+  Color textColor = isDarkMode ? Colors.white : Colors.black;
+  Color subtitleColor = isDarkMode ? Colors.grey[400]! : Colors.grey[700]!;
+  Color predictionColor = item['prediction_score'] < 50 ? Colors.redAccent : Colors.green;
+
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        backgroundColor: isDarkMode ? Colors.grey[900] : Colors.white,
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              item['name'],
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24, color: textColor),
+            ),
+            const SizedBox(height: 10),
+            Image.memory(item['image'], fit: BoxFit.cover),
+            const SizedBox(height: 10),
+            Text(
+              'Plante prédite: ${item['name']}',
+              style: TextStyle(fontWeight: FontWeight.w600, color: textColor),
+            ),
+            Text(
+              'Confiance de la prédiction: ${item['prediction_score']}%',
+              style: TextStyle(color: predictionColor),
+            ),
+            const SizedBox(height: 5),
+            Divider(
+              color: isDarkMode ? Colors.grey[700] : Colors.grey[300],
+              thickness: 2,
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'Date de la photo: $formattedDate à $formattedTime',
+              style: TextStyle(color: subtitleColor),
+            ),
+            Text(
+              'Coordonnées de la photo: ${item['latitude']} ; ${item['longitude']}',
+              style: TextStyle(color: subtitleColor),
+            ),
           ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.delete, color: Colors.red),
+                onPressed: () {
+                  _deleteItem(item['id']);
+                  Navigator.pop(context);
+                },
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Fermer'),
+              ),
+            ],
+          )
+        ],
+      );
+    },
+  );
+}
 
   @override
   Widget build(BuildContext context) {
